952818ed69d35a380256ad698bff12cd9447e521
Oskar Danielsson <oskar@smsgrupp.se>
2014-11-29 02:31:08 -0500

    Replaced database schema file

--- forum.sql
+++ forum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 28, 2014 at 08:52 AM
+-- Generation Time: Nov 29, 2014 at 08:08 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -34,6 +34,14 @@ CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`, `admin`) VALUES
+(1, 'Regular User', 'All regular users are part of this group', 0),
+(2, 'Administrator', 'Message Board Administrators', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -47,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `logins` (
   `ip` varchar(15) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user` (`user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -64,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   PRIMARY KEY (`id`),
   KEY `user` (`user`),
   KEY `topic` (`topic`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=15 ;
 
 -- --------------------------------------------------------
 
@@ -79,7 +87,16 @@ CREATE TABLE IF NOT EXISTS `tables` (
   `user_group_viewing` int(11) NOT NULL DEFAULT '0',
   `user_group_posting` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`id`, `date`, `name`, `user_group_viewing`, `user_group_posting`) VALUES
+(4, '2014-11-29 06:55:56', 'Announcements', 0, 2),
+(5, '2014-11-29 06:55:56', 'General', 0, 0),
+(6, '2014-11-29 06:55:56', 'Trash', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -96,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `topics` (
   PRIMARY KEY (`id`),
   KEY `user` (`user`),
   KEY `table` (`table`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=24 ;
 
 -- --------------------------------------------------------
 
@@ -114,7 +131,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `email` (`email`),
   KEY `default_group` (`default_group`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `default_group`) VALUES
+(7, 'Oskar', '49174e529aa19bf1de200d3594bba6ff', 'oskar.danielsson@hotmail.com', 1);
 
 --
 -- Triggers `users`
@@ -141,6 +165,14 @@ CREATE TABLE IF NOT EXISTS `usersgroups` (
   KEY `userid` (`userid`),
   KEY `groupid` (`groupid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `usersgroups`
+--
+
+INSERT INTO `usersgroups` (`userid`, `groupid`, `moderator`) VALUES
+(7, 1, 1),
+(7, 2, 1);
 
 --
 -- Constraints for dumped tables
