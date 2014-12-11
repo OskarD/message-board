50d7a1e0e28bda37d22790daa94f8c4a8e85afa2
Oskar Danielsson <oskar@smsgrupp.se>
2014-12-11 00:10:18 -0500

    - Added bold text to index page

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 		<p>All passwords are encrypted before they are transmitted anywhere.</p><br />
 		<p>You can find the source of this page at <a class='bold' target='_blank' href="https://bitbucket.org/OskarD/message-board">Bitbucket here</a>.</p><br />
 		<p>You can also visit my regular website at: <a class='bold' href="http://www.OskarD.me/">www.OskarD.me</a></p><br /><br />
-		<p>NEW: Now there is also an, although very basic, Android app available for this forum! <a href='https://play.google.com/store/apps/details?id=me.oskard.finalproject'>Get it here!</a></p>
+		<p>NEW: Now there is also an, although very basic, Android app available for this forum! <a class='bold' href='https://play.google.com/store/apps/details?id=me.oskard.finalproject'>Get it here!</a></p>
 	</div>
 
 	<?php
